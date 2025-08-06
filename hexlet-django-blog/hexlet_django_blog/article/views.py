c89943fcd21376a828385bead8019fc040699310
Greshnik <sanmaximus92@mail.ru>
2025-08-06 07:00:54 +0300

    Шаблонизатор добавлен

--- hexlet-django-blog/hexlet_django_blog/article/views.py
+++ hexlet-django-blog/hexlet_django_blog/article/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
 
 def index(request):
     context = {
-        'app_name': 'article'
+        'app_name': 'hexlet_django_blog.article',
     }
     return render(request, 'articles/index.html', context)
+
